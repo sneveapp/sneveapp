39b09610675ee04949372ef07648ef319a08c558
Marek Lorenz <marek.lorenz@tum.de>
2022-04-22 15:55:12 +0200

    Added swipe feature for navigation in draggable scrollabe sheet

--- lib/components/eventlist_description.dart
+++ lib/components/eventlist_description.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:sneve/services/eventlist.dart';
 
 
 class EventlistDescriptionView extends StatelessWidget {
-  const EventlistDescriptionView({Key? key}) : super(key: key);
+  final EventList _eventList;
+  const EventlistDescriptionView(this._eventList, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SliverList(delegate: SliverChildListDelegate([
-      const Text("Description")
+      Column(children: [Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        alignment: Alignment.topLeft,
+        child: Text(_eventList.getDescription()),
+      ),
+      Container(
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          border: Border.all(color: Colors.grey, width: 0.5)
+        ),
+        alignment: Alignment.topLeft,
+        child: Column(children: const [
+          Text("Author:     "),
+          Text("Created:    "),
+          Text("Saved:      "),
+          Text("Started     ")
+        ]),
+      )
+      
+      ])
     ]));
   }
 }
